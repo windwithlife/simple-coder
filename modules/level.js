6fde841d4c3824532709f3b3fd683429940dd262
zhangyq <379163259@qq.com>
2016-09-10 19:38:23 +0800

    adjust web pages

---
+++ modules/level.js
@@ -0,0 +1,17 @@
+/**
+ * Created by zhangyq on 2016/5/1.
+ */
+
+module.exports  =
+{
+    name: "product",
+    channel:"产品",
+    fields: {
+        id: {type: 'Long', dName: "编号",show:false},
+        name: {type: 'String', dName: "名称",show:true},
+        sex: {type: 'int', dName: "性别" },
+        level: {type: 'int', dName: "性别", refer: {module:"level",map:"ManyToOne"} },
+        age: {type: 'int', dName: "年龄"},
+        pic: {type: 'String', dName: "图片"}
+    }
+}
